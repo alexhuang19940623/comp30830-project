30ed26f0ea4b7def178611a68e5aab526544aba6
Tianyu Huang <tianyu.huang@ucdconnect.ie>
2022-03-04 01:23:10 +0000

    test

--- insert.py
+++ insert.py
@@ -26,6 +26,8 @@ engine.execute(sql)
 
 for res in engine.execute("SHOW VARIABLES"):
     print(res)
+r = requests.get(STATIONS, params={"apiKey": KEY,"contract": NAME})
+
 
 sql1 = """
 use dbikes;
@@ -53,3 +55,35 @@ try:
     print(res.fetchall())
 except Exception as e:
     print(e)
+
+def write_to_file(text):
+    f = open("bikes__{}".format(now).replace(" ", "_").replace(":", "=").replace(".", ","), "w")
+    f.write(r.text)
+    f.close()
+
+
+def stations_to_db(text):
+    stations = json.loads(text)
+    print(type(stations), len(stations))
+    for station in stations:
+        print(station)
+        vals = (
+            station.get('address'), int(station.get('banking')), station.get('bike_stands'), int(station.get('bonus')), 
+            station.get('contract_name'), station.get('name'), station.get('number'), 
+            station.get('position').get('lat'), station.get('position').get('lng'), station.get('status')
+        )
+        engine.execute("insert into station values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",vals)
+        break
+    return
+
+
+while True:
+    try:
+        now = datetime.datetime.now()
+        r = requests.get(STATIONS, params={"apiKey": KEY, "contract": NAME})
+        print(r, now)
+        write_to_file(r.text)
+        stations_to_db(r.text)
+        time.sleep(5*60)
+    except:
+        print(traceback.format_exc())
